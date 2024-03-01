7ebe11ec99c54c53324f9053769fc61a28ba1e6f
Liam Darkmoon <liamdarkmoon@gmail.com>
2024-03-01 15:37:56 -0300

    mobile v1.5

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
         <div className='lnd pg container-fluid d-flex flex-column align-items-center justify-content-evenly'>
             <NavBar/>
             <div className='row py-5 align-items-center justify-content-between'>
-                <div className='col-8 col-sm-5 text-center'>
+                <div className='col-12 col-sm-5 text-center'>
                     <h1 className='lnd-hl'> The playground to make your fantasy real </h1>
                     <div className='separator'></div>
                     <h4 className='fw-regular'> Roll your dice with a click and ready your character for the adventure </h4>
